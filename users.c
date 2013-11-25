907c8d7e1241b79d7bbbc41982282bbcebb088f8
Justin Slind <justin.slind@gmail.com>
2013-11-24 23:56:06 -0700

    tokenize user command and param

--- users.c
+++ users.c
@@ -3,8 +3,11 @@
 
 void cmd_user(char *cmd, session_info_t *si) {
 
+	char username[USER_STRLEN];
 	//tokenize to get parameter
-
+	char *token = strtok(cmd, " ");
+	token = strtok(NULL," ");
+	strcpy(username,token);
 	//check if user exisits in file
 
 	//set session info user to username
