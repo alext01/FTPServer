1d9c669d3dfad5fc7efb528788b4075e42d99f16
Alex Tai <taia3@students.cs.macewan.ca>
2013-11-26 19:32:22 -0700

    Fixed a bunch of errors and warnings in filemanip.c

--- filemanip.c
+++ filemanip.c
@@ -1,43 +1,64 @@
+/****************************************************************
+ *
+ * filemanip.c
+ * Group Project
+ * Class: CMPT 361
+ * Version: 1.2ftp
+ * Author: Alex Tai
+ * Includes function such as reading files, finding size of the
+ * file, writing to a file, listing directories, changing
+ * directories.
+ *
+ ***************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <errno.h>
-//#include <ctype.h>
-//#include <unistd.h>
+#include <string.h>
 
 #include "filemanip.h"
-#include "common.h"
+//#include "common.h"
 
 #define MAX_FDATSZ 4096
 
-extern int verbflg;
-extern byte_t fileBuff[MAX_FDATSZ];
+//extern int verbflg;
+char fileBuff[MAX_FDATSZ];
 
-FILE * openFile(char * fileName, char * purp){
+FILE * openFile(char * fileName, char * path, char * purp){
   FILE *fp;
 
   if((fp = fopen(fileName, purp)) == NULL)
     {
-      if(verbflg)
-	fprintf(stderr, "Cannot open file: %s\n", fileName);
-      else
-	fprintf(stderr, "Error");
-      exit(1);
+      fprintf(stderr, "Cannot open file: %s\n", fileName);
+      return NULL;
     }
 
   printf("file open success\n");
   return fp;
 }
 
+int closeFile(FILE *fp){
+  int errchk;
+  errno = 0;
+
+  errchk = fclose(fp);
+  if(errno){
+    fprintf(stderr, "Error in function closeFile: %s\n", strerror(errno));
+    return -1;
+  }
+  return 0;
+}
 
 long int fSzCount(FILE * fp){
   long int fileSZ = 0;
   int errck;
+  errno = 0;
 
   if( (errck = fseek(fp, 0, SEEK_END)) == -1){
     fprintf(stderr, "Error: seeking EOF\n");
-    exit(1);
+    return -1;
   }
+
   fileSZ = ftell(fp);
   rewind(fp);
 
@@ -80,26 +101,46 @@ void writeFile(FILE * fp, long int datSZ){
 
 char * listDirect(char * curloc){
   char * directory;
-  DIR *dp;
-  sturct dirent *ep;
+  DIR *dp;  //directory pointer
+  struct dirent *ep;
 
+  errno = 0;
   dp = opendir(curloc);
 
-  if(dp != NULL){
-    while(ep = readdir(dp)){
+  if(dp == NULL){
+    fprintf(stderr, "Error: %s\n", strerror(errno));
+  }
+
+  while( (ep = readdir(dp)) ){
       puts(ep->d_name);
-    }
 
     //checks to see if its and error or eof
-    if(errno){
-      fprintf(stderr, "Error: %s\n", strerror(errno));
-      exit(0);
+    if(ep == NULL){
+      if(errno){
+	fprintf(stderr, "Error: %s\n", strerror(errno));
+	return NULL;
+      }
     }
-  }
-  else{
-    fprintf(stderr, "Error opening directory\n");
-    return "err";
   }
 
   return directory;
+}
+
+
+char * changeDirect(char * curloc, char * directChanges){
+  char newPath[MAX_FDATSZ];
+  int changeSz;
+  int curlocSz;
+
+  if(directChanges[0] == '~'){
+    return directChanges;
+  }
+  else if(directChanges[0] == '.'){
+    if(directChanges[1] == '.'){
+    }
+    else{
+    }
+  }
+  
+  return newPath;
 }
